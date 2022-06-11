ddbe179f10be70b706dd275490bad8a9e80c2b2e
Dmitrii <melomanko@gmail.com>
2022-06-11 21:11:14 +0700

    added currencies and rebuided layout w bootstrap

--- src/App.js
+++ src/App.js
@@ -1,49 +1,19 @@
 import React, { useEffect, useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import { Row, Col, FloatingLabel } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
 import './App.css';
 
-const App = (props) => {
+const App = () => {
 	const [currentPrice, setPrice] = useState('0');
 	const [valueFromInput, setValueFromInput] = useState('');
 	const [data, setData] = useState([]);
 
-const objCurrencies = [
-		{
-			ID: 'R01235',
-			NumCode: '840',
-			CharCode: 'USD',
-			Nominal: 1,
-			Name: 'Доллар США',
-			Value: 58.3895,
-			Previous: 60.2282,
-		},
-		{
-			ID: 'R01239',
-			NumCode: '978',
-			CharCode: 'EUR',
-			Nominal: 1,
-			Name: 'Евро',
-			Value: 62.0934,
-			Previous: 63.938,
-		},
-		{
-			ID: 'R01010',
-			NumCode: '036',
-			CharCode: 'AUD',
-			Nominal: 1,
-			Name: 'Австралийский доллар',
-			Value: 41.9003,
-			Previous: 43.3763,
-		},
-		{
-			ID: 'R01060',
-			NumCode: '051',
-			CharCode: 'AMD',
-			Nominal: 100,
-			Name: 'Армянских драмов',
-			Value: 13.6792,
-			Previous: 13.9809,
-		},
-	];
+	let newArr = [];
+
+	for (let value in data.Valute) {
+		newArr.push(data.Valute[value]);
+	}
 
 	useEffect(() => {
 		const getRequest = async () => {
@@ -55,41 +25,56 @@ const objCurrencies = [
 		getRequest();
 	}, []);
 
-	const Convert = (e) => {
-		// const nameCurrency = e.target.getAttribute('curr');
-		const nameCurrency = document.getElementById('menu1').value;
+	const Convert = () => {
+		let nameCurrency;
+
+		if (document.getElementById('menu').value === 'undefined') {
+			return;
+		} else {
+			nameCurrency = document.getElementById('menu').value;
+		}
+
 		const currentValue = data.Valute[nameCurrency].Value;
 		const valuteName = data.Valute[nameCurrency].Name;
 		const conversion = (valueFromInput / currentValue).toFixed(2);
+
 		setPrice(`${conversion} ${valuteName}`);
 	};
 
 	return (
-		<div className="app">
+		<Container className="container" fluid="xl">
+			<h2>Currency Converter</h2>
 			<div className="counter">{currentPrice}</div>
-			<div className="value-input">
-				<input
-					type="number"
-					className="form-control"
-					placeholder="Write the amount in RUB"
-					onChange={(e) => setValueFromInput(+e.target.value)}
-				/>РУБ.
-				<br />
+			<div className="select-curr mt-3">
+				<Row className="g-2">
+					<Col md>
+						<FloatingLabel
+							onChange={(e) => {
+								setValueFromInput(+e.target.value);
+								setPrice();
+							}}
+							controlId="floatingInputGrid"
+							label="Write the amount in RUB">
+							<Form.Control type="number" placeholder="Write the amount in RUB" />
+						</FloatingLabel>
+					</Col>
+					<Col md>
+						<FloatingLabel controlId="floatingSelectGrid" label="Check the Currency">
+							<Form.Select onChange={Convert} id="menu" aria-label="Check the Currency">
+								<option value={'undefined'}>Open this select menu</option>
+								{newArr.map(({ CharCode, ID, Name }, i) => {
+									return (
+										<option key={ID} value={CharCode}>
+											{i + 1}. {CharCode} - {Name}
+										</option>
+									);
+								})}
+							</Form.Select>
+						</FloatingLabel>
+					</Col>
+				</Row>
 			</div>
-			<div className="select-curr">
-				<select type="text" id="menu1" onChange={(e) => Convert(e)}>
-					<option value={''}>Check the Currency</option>
-					{objCurrencies.map(({ CharCode, Name, ID }) => {
-						return (
-							<option key={ID} curr={CharCode} value={CharCode}>
-								{CharCode} {Name}
-							</option>
-						);
-					})}
-				</select>
-				{/* <p>текущий курс: {}</p> */}
-			</div>
-		</div>
+		</Container>
 	);
 };
 
