8535e6a784b728f321e1bece1a4145c627eb9899
Dmitrii <melomanko@gmail.com>
2022-06-13 23:16:59 +0700

    getRequest func refactor w axios

--- src/App.js
+++ src/App.js
@@ -2,41 +2,43 @@ import React, { useEffect, useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import { Row, Col, FloatingLabel } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
+import axios from 'axios';
+
 import './App.css';
 
 const App = () => {
 	const [currentPrice, setPrice] = useState('0');
-	const [valueFromInput, setValueFromInput] = useState('');
-	const [data, setData] = useState([]);
+	const [inputValue, setInputValue] = useState('');
+	const [response, setResponse] = useState([]);
 
-	let newArr = [];
+	let currenciesArray = [];
 
-	for (let value in data.Valute) {
-		newArr.push(data.Valute[value]);
+	for (let value in response.Valute) {
+		currenciesArray.push(response.Valute[value]);
 	}
 
-	const getRequest = async () => {
-		const request = await fetch('https://www.cbr-xml-daily.ru/daily_json.js');
-		const response = await request.json();
-		setData(response);
+	const getRequest = () => {
+		const _url = 'https://www.cbr-xml-daily.ru/daily_json.js';
+		axios
+			.get(_url)
+			.then((response) => {
+				return setResponse(response.data);
+			})
+			.catch((error) => console.log(error.code, error.message));
 	};
 
 	useEffect(() => {
 		getRequest();
 	}, []);
 
-	const Convert = () => {
-		const menu = document.getElementById('menu').value;
-		let nameCurrency;
-		if (!menu) {
+	const convert = () => {
+		const currencyName = document.getElementById('menu').value;
+		if (!currencyName) {
 			return;
-		} else {
-			nameCurrency = menu;
 		}
-		const currentValue = data.Valute[nameCurrency].Value;
-		const valuteName = data.Valute[nameCurrency].Name;
-		const conversion = Number((valueFromInput / currentValue).toFixed(2)).toLocaleString('ru');
-
+		const currentValue = response.Valute[currencyName].Value;
+		const valuteName = response.Valute[currencyName].Name;
+		const conversion = Number((inputValue / currentValue).toFixed(2)).toLocaleString('ru');
 		setPrice(`${conversion} ${valuteName}`);
 	};
 
@@ -50,10 +52,10 @@ const App = () => {
 						<FloatingLabel
 							onChange={(e) => {
 								// wokr with integer numbers only
-								e.target.value = e.target.value.replace(/[^\d]/g, '').replace(/\B(?=(?:\d{3})+(?!\d))/g, ' ');
-								setValueFromInput(String(e.target.value).replace(/\s/g, ''));
+								e.target.value = new Intl.NumberFormat('ru-RU').format(e.target.value.replace(/[^\d]/g, ''));
+								setInputValue(String(e.target.value).replace(/\s/g, ''));
 							}}
-							onKeyUp={Convert}
+							onKeyUp={convert}
 							controlId="floatingInputGrid"
 							label="Write the amount in RUB">
 							<Form.Control type="text" placeholder="Write the amount in RUB" />
@@ -61,15 +63,19 @@ const App = () => {
 					</Col>
 					<Col md>
 						<FloatingLabel controlId="floatingSelectGrid" label="Check the Currency">
-							<Form.Select onChange={Convert} id="menu" aria-label="Check the Currency">
+							<Form.Select onChange={convert} id="menu" aria-label="Check the Currency">
 								<option value={''}>Open this select menu</option>
-								{newArr.map(({ CharCode, ID, Name }, i) => {
-									return (
+								{currenciesArray.length > 0 ? (
+									currenciesArray.map(({ CharCode, ID, Name }, i) => (
 										<option key={ID} value={CharCode}>
 											{i + 1}. {CharCode} - {Name}
 										</option>
-									);
-								})}
+									))
+								) : (
+									<option value={''} disabled>
+										There is no data!
+									</option>
+								)}
 							</Form.Select>
 						</FloatingLabel>
 					</Col>
